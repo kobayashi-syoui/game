8f4be06076928e2f48cf3080fc222a03d72e62d9
kobayashi.syoui <syoi.kobayashi@gu3.co.jp>
2012-04-11 15:16:42 +0900

    e

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Game::Application.config.secret_token = 'a4f134369f160a47aa548ed74820dae4c873c7acfcd4da5814da99d51601faea9a1b09dc04b45d12bfa1cad4ebbd304fc9e305493eacf4dd7d0953e89736885d'
