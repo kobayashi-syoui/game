6a0b9b88ad6230b3e0177f1f5fbce1756f96f041
川上 泰生 <kawakamitaiki@kawakami-yasuo-no-MacBook-Air.local>
2012-07-06 00:31:51 +0900

    add creature class

--- public/html/js/battle.js
+++ public/html/js/battle.js
@@ -4,22 +4,18 @@ jQuery(function($) {
     var slideXOld = 0;
     var slideYOld = 0;
     var i = 0;
-    	var points = []; //ドラッグ時のマウスの座標を集める
+    var points = []; //ドラッグ時のマウスの座標を集める
 	var $canvas; //追加されたキャンバスを格納
 	var canvasWidth;
 	var canvasHeight;
 	var aw = 10;
 	var dragFlag = false;
 
-	/**
-	 * 2つの座標と幅から4つ(面)の座標を得る
-	 * @param ptax
-	 * @param ptay
-	 * @param ptbx
-	 * @param ptby
-	 * @param width
-	 */
     $('body').css({'width': w,'height': h});
+    
+    
+    
+    
    
 	$('#fieldWrap').bind({
 		'touchstart': function(e) {
@@ -46,17 +42,15 @@ jQuery(function($) {
     	}
    });
    
-   $('#field1').bind({
+   $('.myCreature').bind({
 		'touchstart': function(e) {
 			dragFlag = true;
 			$canvas = addCanvas();
 			points = [{x:event.changedTouches[0].pageX, y:event.changedTouches[0].pageY}];
-
     	},
     	'touchmove': function(e) {
 			e.preventDefault();
 			var ctx = $canvas.get(0).getContext('2d');
-			var off = $("#field6").offset();
 		    points.push({x:event.changedTouches[0].pageX, y:event.changedTouches[0].pageY});
 		
 		    var array = getRectPoints(
@@ -77,26 +71,14 @@ jQuery(function($) {
 		    ctx.lineTo(array[6].x, array[6].y);
 		    ctx.closePath();
 		    ctx.fill();
-		    /*
-			if (event.changedTouches[0].pageX > off.left && event.changedTouches[0].pageX < off.left + 40  && event.changedTouches[0].pageY > off.top  && event.changedTouches[0].pageY < off.top + 40) {
-				$("#field6").addClass("selectObj");
-			}
-			
-			else {
-				$("#field6").removeClass("selectObj");
-			}
-			*/
 			isHoverTarget(event);
 		},
 		'touchend': function(e) { 
+			var myCreatureId = $(this).attr("id");
 			$canvas.remove();
 			points = [];
 			dragFlag = false;
-			var off = $("#field6").offset();
-			if (event.changedTouches[0].pageX > off.left && event.changedTouches[0].pageX < off.left + 40  && event.changedTouches[0].pageY > off.top && event.changedTouches[0].pageY < off.top + 40) {
-				alert("あ");
-				$("#field6").removeClass("selectObj");
-			}
+			isSelectTarget(event, myCreatureId);
 		}
 	});
 
@@ -117,8 +99,6 @@ jQuery(function($) {
 		array.push({x:ptbx - Uy* w  - Ux* h, y:ptby + Ux* w  - Uy* h});
 		array.push({x: ptbx  - Uy*aw - Ux* h, y:ptby + Ux*aw - Uy* h});
 		array.push({x:ptax - Uy*aw, y:ptay  + Ux*aw});
-		
-		
 
 		return array;
 	}
@@ -131,16 +111,61 @@ jQuery(function($) {
 	
 	 //ターゲットに照準が当たっているかの判別
 	function isHoverTarget(e) {
+			// ターゲットが敵クリーチャーの場合
+			for (i = 1;i <= $(".targetCreature").length; i++){
+				var t = i + $(".targetCreature").length;
+				var obj = new creature("#field" + t);
+				if (e.changedTouches[0].pageX > obj.x() && e.changedTouches[0].pageX < obj.x() + 40  && e.changedTouches[0].pageY > obj.y() && e.changedTouches[0].pageY < obj.y() + 40) {
+					$("#field" + t).addClass("selectObj");
+				}
+				else {
+					$("#field" + t).removeClass("selectObj");
+				}
+			}
+	}
+	 //ターゲットを選択したかの判別
+	function isSelectTarget(e, name) {
+		// ターゲットが敵クリーチャーの場合
 		for (i = 1;i <= $(".targetCreature").length; i++){
 			var t = i + $(".targetCreature").length;
-			var off = $("#field" + t).offset();
-			if (e.changedTouches[0].pageX > off.left && e.changedTouches[0].pageX < off.left + 40  && e.changedTouches[0].pageY > off.top  && e.changedTouches[0].pageY < off.top + 40) {
-				$("#field" + t).addClass("selectObj");
-			}
-			
-			else {
+			var obj = new creature("#field" + t);
+			if (e.changedTouches[0].pageX > obj.x() && e.changedTouches[0].pageX < obj.x() + 40  && e.changedTouches[0].pageY > obj.y() && e.changedTouches[0].pageY < obj.y() + 40) {
+				
 				$("#field" + t).removeClass("selectObj");
+				$("#"+name).css("-webkit-transform","translate3d("+ (obj.fieldX() - 45) +"px,"+obj.fieldY()+"px,"+obj.fieldZ()+"px)");
+				//alert("ドゴォ!"+ name + "からの攻撃で" + $("#field" + t).attr("id") + "は死んだ");
 			}
 		}
-	} 
+	}
+	//ここからクリーチャーの情報
+	function creature(id) {
+		this.x = function() {
+			return $(id).offset().left;
+		};
+
+		this.y = function() {
+			return $(id).offset().top;
+		};
+		this.fieldX = function() {
+			m = new WebKitCSSMatrix($(id).css('-webkit-transform'));
+			return m.m41;
+		};
+
+		this.fieldY = function() {
+			m = new WebKitCSSMatrix($(id).css('-webkit-transform'));
+			return m.m42;
+		};
+		
+		this.fieldZ = function() {
+			m = new WebKitCSSMatrix($(id).css('-webkit-transform'));
+			return m.m43;
+		};
+		/*
+		var off = $(id).offset();
+		alert(off.left);
+		var m = new WebKitCSSMatrix($(id).css('-webkit-transform'));
+		var obj = {x:off.left, y:off.top, dx:m.m41, dy:m.m42, dz:m.m43};
+		return obj;
+		*/
+	}
 });
